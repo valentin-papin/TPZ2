cf32a2f4eab5c222a282be12dfa322b2d13a2432
valentin-papin <valentin.papin1@gmail.com>
2017-11-13 16:46:41 +0100

    Push fin de séance du 13/11

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -2,19 +2,43 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Person;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class PersonController extends Controller
 {
-    public function new()
-    {
-        return new Response('<html><body>Hello World !</body></html>');
+    public function new(Request $request){
+        $em = $this->getDoctrine()->getManager();
+        $personne = new Person();
+
+        $form=  $this->createFormBuilder($personne)
+            ->add('name',TextType::class)
+            ->add('age',IntegerType::class)
+            ->add("visible", CheckboxType::class)
+            ->add('color', TextType::class)
+            ->add('sumbitPersonne', SubmitType::class, array('label'=>'Créer'))
+            ->getForm();
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $personne->setCreatedAt(new \DateTime('now'));
+            $em->persist($personne);
+            $em->flush();
+        }
+
+        return $this->render("Person/new.html.twig",array('form'=>$form->createView()));
     }
 
-    public function index()
-    {
-        return new Response('<html><body>Hello World !</body></html>');
+    public function index(){
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository(Person::class);
+        $personnes = $repo->findAll();
+        return $this->render("Person/index.html.twig", ["personnes"=>$personnes]);
     }
 }
--- src/Entity/Person.php
+++ src/Entity/Person.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
  * Class Person
  * @package TP2\Entity
  * @ORM\Entity
+ * @ORM\Table(name="valentin_personnes")
  */
 class Person
 {
@@ -36,12 +37,110 @@ class Person
     protected $visible;
     /**
      * @var string
-     * @ORM\Column(type="date")
+     * @ORM\Column(type="date", name="created_at")
      */
-    protected $created_at;
+    protected $createdAt;
     /**
      * @var string
      * @ORM\Column(type="string", length=50)
      */
     protected $color;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param int $age
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isVisible()
+    {
+        return $this->visible;
+    }
+
+    /**
+     * @param boolean $visible
+     */
+    public function setVisible($visible)
+    {
+        $this->visible = $visible;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param string $created_at
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return string
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * @param string $color
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+
 }
